d4e30f9681b78f0a18e6b11ae1384107f5fc6929
Felipe Gutierrez <fgutierrez3@wisc.edu>
2015-08-17 18:03:49 -0500

    MeshFileToPointCloud

--- js/RenderMesh.js
+++ js/RenderMesh.js
@@ -7,8 +7,8 @@ function RenderMesh()
     //CurrGeometry = "Cube";
     //MeshGeometry = new THREE.BoxGeometry(1,2,1);
     /* Sphere */
-    //CurrGeometry = "Sphere";
-    //MeshGeometry = new THREE.SphereGeometry(1);
+    CurrGeometry = "Sphere";
+    MeshGeometry = new THREE.SphereGeometry(1);
     /* Cylinder radiusTop, radiusBottom, radiusHeight, radiusSegments, heightSegments*/
     // CurrGeometry = "Cylinder";
     // MeshGeometry = new THREE.CylinderGeometry(0.5,0.5,1,32);
@@ -22,8 +22,77 @@ function RenderMesh()
     //CurrGeometry = "Torus";
     //MeshGeometry = new THREE.TorusGeometry(1,0.2,16,100);
     /* Torus knot */
-    CurrGeometry = "TorusKnot";
-    MeshGeometry = new THREE.TorusKnotGeometry(1,0.2,100,16);
+    //CurrGeometry = "TorusKnot";
+    //MeshGeometry = new THREE.TorusKnotGeometry(1,0.2,100,16);
+
+
+    MeshGeometry.computeBoundingBox();
+    console.log(MeshGeometry);
+    var MeshMaterial = new THREE.MeshNormalMaterial();
+    console.log("MeshMaterial");
+    MeshMaterial.side = THREE.DoubleSide;
+
+    console.log(MeshMaterial);
+    MeshObject = new THREE.Mesh(MeshGeometry, MeshMaterial);
+    MeshObject.overdraw = true;
+    console.log(MeshObject);
+
+    var box = new THREE.BoxHelper( MeshObject );
+
+    MeshScene.add(MeshObject);
+    MeshScene.add(box);
+
+    var max = MeshGeometry.boundingBox.max;
+    var min = MeshGeometry.boundingBox.min;
+    var MaxBoundingBoxHTML= "<p><strong>Max BoundingBox Coordinates:</strong>  x = " +  max.x + ",   y = " + max.y + ",   z = " + max.z + "</p>";
+    var MinBoundingBoxHTML= "<p><strong>Min BoundingBox Coordinates:</strong>  x = " +  min.x + ",   y = " + min.y + ",   z = " + min.z + "</p>";
+    $("#DebugInfo").html(MaxBoundingBoxHTML + MinBoundingBoxHTML);
+
+    MeshAnimate();
+}
+
+function RenderMesh(filename)
+{
+    InitScene(MeshRenderer, MeshScene, MeshCamera, MeshControls, MeshAxisHelper);
+    $MeshContainer.append(MeshRenderer.domElement);
+
+
+    CurrGeometry = "Sphere";
+    MeshGeometry = new THREE.SphereGeometry(1);
+
+    /*** Texture Loading ***/
+    var manager = new THREE.LoadingManager();
+    manager.onProgress = function ( item, loaded, total ) {
+      console.log( item, loaded, total );
+    };
+    var texture = new THREE.Texture();
+                var onProgress = function ( xhr ) {
+                    if ( xhr.lengthComputable ) {
+                        var percentComplete = xhr.loaded / xhr.total * 100;
+                        console.log( Math.round(percentComplete, 2) + '% downloaded' );
+                    }
+                };
+
+                var onError = function ( xhr ) {
+                };
+    var loader = new THREE.OBJLoader( manager );
+
+            loader.load( 'meshes/dragon.obj', function ( object ) {
+
+                    object.traverse( function ( child ) {
+
+                        if ( child instanceof THREE.Mesh ) {
+
+                            child.material.map = texture;
+
+                        }
+
+                    } );
+
+                    //object.position.y = - 80;
+                    scene.add( object );
+
+                }, onProgress, onError );
 
 
     MeshGeometry.computeBoundingBox();
